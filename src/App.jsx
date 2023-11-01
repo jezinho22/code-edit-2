65c2cd4b89943bc9668dbed38865d37cc61f81c4
Jeremy Johns <jez.johns@gmail.com>
2023-11-01 19:58:58 +0000

    added babel and onupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Editor from "./components/Editor";
 import "./App.css";
+import "./components/editor.css";
 
 function App() {
 	return (
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,20 +2,32 @@ import { basicSetup } from "codemirror";
 import { EditorState } from "@codemirror/state";
 import { EditorView, keymap } from "@codemirror/view";
 import { defaultKeymap, indentWithTab } from "@codemirror/commands";
-import { oneDark } from '@codemirror/theme-one-dark'
-import  { javascript } from '@codemirror/lang-javascript'
+import { oneDark } from "@codemirror/theme-one-dark";
+import { javascript } from "@codemirror/lang-javascript";
 
-
-
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 export default function Editor() {
 	const editor = useRef();
+	const [code, setCode] = useState("");
+
+	// create event listener and handler on EditorView
+	// when v changes, set code state to this
+	const onUpdate = EditorView.updateListener.of((v) => {
+		setCode(v.state.doc.toString());
+		console.log(v.state.doc.toString());
+	});
 
 	useEffect(() => {
 		const startState = EditorState.create({
 			doc: "Hello World",
-			extensions: [javascript(), oneDark, basicSetup, keymap.of([defaultKeymap, indentWithTab])],
+			extensions: [
+				javascript(),
+				oneDark,
+				basicSetup,
+				keymap.of([defaultKeymap, indentWithTab]),
+				onUpdate,
+			],
 		});
 
 		const view = new EditorView({ state: startState, parent: editor.current });
@@ -25,5 +37,9 @@ export default function Editor() {
 		};
 	}, []);
 
-	return <div ref={editor}></div>;
+	return (
+		<div className="editor-container">
+			<div ref={editor}></div>
+		</div>
+	);
 }
